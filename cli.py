6d4368787339c9965677b2c2707b50204607c63a
ochieng <ochieng.oduor@student.moringaschool.com>
2025-08-28 14:57:34 +0300

    creating a coffe shop cli menu

--- cli.py
+++ cli.py
@@ -1,3 +1,19 @@
 from lib.helpers import *
 from lib.models import session
 
+
+
+
+def main_menu():
+    current_customer = None
+
+    while True:
+        print("/n--- coffee Shop CLI ---")
+        print("1. View menu")
+        print("2. Register customer")
+        print("3. Select customer")
+        if current_customer:
+            print(f"4. Place order ({current_customer.name})")
+            print("5. View order history")
+        print("6. View all orders")
+        print("7. Exit")
