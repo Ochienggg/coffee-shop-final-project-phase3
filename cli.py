294deb0b46cf0a64732a789207fb7498fd5e8dfd
ochieng <ochieng.oduor@student.moringaschool.com>
2025-08-28 15:05:00 +0300

    Defining the options for menu items

--- cli.py
+++ cli.py
@@ -17,3 +17,20 @@ def main_menu():
             print("5. View order history")
         print("6. View all orders")
         print("7. Exit")
+
+        choice = input("Choose option:").strip()
+
+        if choice =="1":
+            menu_items = get_all_menu_items()
+            if menu_items:
+                print("\n---Menu---")
+                for item in menu_items:
+                    print(f"{item.id}. {item.name}")
+                    print (f"{item.desciption}")
+            else:
+                print("No menu items found")
+        elif choice== "2":
+            name = input("Enter your name:").strip()
+            email = input("Enter your email").strip()
+
+            if 
